504378ce2627ab73c30744e3074c23d2f182f681
baisali609 <baisali609@gmail.com>
2025-02-11 12:17:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //complete this code
 class Person {
 	constructor(name, age) {
-        this.#name = name;
-        this.#age = age;
+        this._name = name;
+        this._age = age;
     }
 
     get name() {
-        return this.#name;
+        return this._name;
     }
 
     get age() {
-        return this.#age;
+        return this._age;
     }
 
-    set age(age) {
-            this.#age = age;
+    set age(newage) {
+            this._age = newage;
     }
 }
 
